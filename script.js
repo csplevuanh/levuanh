a867b28d7a9dcc5f74d5d1c6e062fa1dd4b70869
Vu-Anh Le <83645037+csplevuanh@users.noreply.github.com>
2024-05-24 16:00:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchIcon = document.getElementById('search-icon');
+const searchInput = document.getElementById('search-input');
+
+searchIcon.addEventListener('click', function() {
+    searchInput.focus();
+});
