2ccad3b98ddd9a635291881f79595b22db7067f2
Vu-Anh Le <83645037+csplevuanh@users.noreply.github.com>
2024-05-24 23:49:31 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const searchIcon = document.getElementById('search-icon');
-const searchInput = document.getElementById('search-input');
-
-searchIcon.addEventListener('click', function() {
-    searchInput.focus();
-});
